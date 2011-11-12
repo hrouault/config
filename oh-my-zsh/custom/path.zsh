575d116a876bbbee60c083d975c8777fe1ed5c1b
Hervé Rouault <herve.rouault@pasteur.fr>
2011-11-12 22:17:30 +0100

    add custom files

---
+++ oh-my-zsh/custom/path.zsh
@@ -0,0 +1,1 @@
+path=(/Users/rouault/arm-cs-tools/bin /usr/local/Cellar/texinfo/4.13a/bin /usr/local/share/python /usr/local/Cellar/ruby/1.9.2-p180/bin /usr/local/sbin /usr/local/bin /usr/texbin $path)
---
+++ oh-my-zsh/custom/shortcut.zsh
@@ -0,0 +1,15 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+
+imogene=/Users/rouault/postdoc/sequence/imogene
+evo=/Users/rouault/these/evolution/paper
+cell=/Users/rouault/postdoc/cellmech/paper
+pre=/Users/rouault/postdoc/cellmech/pre-paper
+notch=/Users/rouault/postdoc/notchpatterning
+mobyle=/Library/WebServer/Documents/mobyle/
+coyotte=/Users/rouault/postdoc/tempcontrol/coyotte
+
+alias gvimdiff="mvim -d"
+alias info="info --vi-keys"
+alias df="df -h"
